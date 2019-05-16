7ea21655cb17b261e959491c894001e920d44edb
ricky_huangyunqi <huangyunqi2017>
2019-05-17 07:37:43 +0800

    add vuex

---
+++ src/store/modules/HelloWorldStore.js
@@ -0,0 +1,23 @@
+// initial state
+const state = {
+        userUrl: ""
+    },
+    /* eslint-disable  no-unused-vars*/
+    actions = {
+        setUerUrl ({ commit, state }, text) {
+
+            commit ("onSetUerUrl", text);
+        }
+    },
+    mutations = {
+        onSetUerUrl (state, text) {
+            state.userUrl = text;
+        }
+    };
+
+export default {
+    namespaced: true,
+    state,
+    actions,
+    mutations
+};
---
+++ src/store/modules/NotFoundStore.js
@@ -0,0 +1,23 @@
+// initial state
+const state = {
+        notFoundText: ""
+    },
+    /* eslint-disable  no-unused-vars*/
+    actions = {
+        setText ({ commit, state }, text) {
+
+            commit ("onSetNotFound", text);
+        }
+    },
+    mutations = {
+        onSetNotFound (state, text) {
+            state.notFoundText = text;
+        }
+    };
+
+export default {
+    namespaced: true,
+    state,
+    actions,
+    mutations
+};
